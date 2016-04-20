52f0066623a4fc40c4e54775c9e3909bd75a0e56
yeibercano <yeibercano@gmail.com>
2016-04-20 15:49:06 -0700

    completes Unit Tests

--- test/unit/utilities.js
+++ test/unit/utilities.js
@@ -8,10 +8,17 @@ describe('the todo.App', function() {
     it('should have all the necessary methods', function(){
       todo.init.should.be.a('function');
       todo.setup.should.be.a('function');
-      expect(todo).to.have.property('setup');
+      expect(todo).to.have.property('init');
+      assert.typeOf(todo, 'object', 'todo is an object - suckah!'); 
     });
   });
 });
 
 describe('the todo.util methods', function() {
+
+  it('should have all the necessary methods', function() {
+    expect(todo.util.getUniqueId()).to.equal(1);
+    expect(todo.util.getUniqueId()).not.to.equal(3);
+    expect(Object.keys(todo.util)).to.have.length(3);
+  })
 });
