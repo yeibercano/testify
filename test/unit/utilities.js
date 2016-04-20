abd7b33ca504e63f5e030f2f2843d984c27a0bc6
yeibercano <yeibercano@gmail.com>
2016-04-20 14:21:39 -0700

    Should tests pass for first unit test

--- test/unit/utilities.js
+++ test/unit/utilities.js
@@ -6,6 +6,9 @@ describe('the todo.App', function() {
   describe('the todo object', function(){
 
     it('should have all the necessary methods', function(){
+      todo.init.should.be.a('function');
+      todo.setup.should.be.a('function');
+      expect(todo).to.have.property('setup');
     });
   });
 });
